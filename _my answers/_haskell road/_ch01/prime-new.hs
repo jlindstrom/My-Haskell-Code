694a9870b003d956cb94149b333ac630beeaee1e
Jeff Lindstrom <jeff@jefflindstrom.com>
2013-10-24 23:55:04 -0700

    Exercises 1.5 to 1.7 answered and associated prime-new.hs file updated.

--- _my answers/_haskell road/_ch01/prime-new.hs
+++ _my answers/_haskell road/_ch01/prime-new.hs
@@ -1,2 +1,13 @@
+divides :: Integer -> Integer -> Bool
 divides d n = rem n d == 0
+
+ldf k n | divides k n = k
+ldf k n | k^2 > n     = n
+ldf k n               = ldf (k+1) n
+
+ld n = ldf 2 n
+
+prime0 n | n <  1    = error "not a positive integer"
+         | n == 1    = False
+         | otherwise = ld n == n
 
