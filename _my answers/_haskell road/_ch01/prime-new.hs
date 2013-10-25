3824a977d9038886031b91820d1f953f3afb0619
Jeff Lindstrom <jeff@jefflindstrom.com>
2013-10-25 00:06:04 -0700

    Type declarations added for ldf, ld, and prime0 functions in prime-new.hs. Exercise 1.9 (question not answer yet) added to answers file.

--- _my answers/_haskell road/_ch01/prime-new.hs
+++ _my answers/_haskell road/_ch01/prime-new.hs
@@ -1,12 +1,15 @@
 divides :: Integer -> Integer -> Bool
 divides d n = rem n d == 0
 
+ldf :: Integer -> Integer -> Integer
 ldf k n | divides k n = k
 ldf k n | k^2 > n     = n
 ldf k n               = ldf (k+1) n
 
+ld :: Integer -> Integer
 ld n = ldf 2 n
 
+prime0 :: Integer -> Bool
 prime0 n | n <  1    = error "not a positive integer"
          | n == 1    = False
          | otherwise = ld n == n
