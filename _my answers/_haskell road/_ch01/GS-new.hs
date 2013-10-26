0e056ed2d21ed95309fd74d58a76e7cdf3b18e7e
Jeff Lindstrom <jeff@jefflindstrom.com>
2013-10-25 22:36:59 -0700

    Slight rearrangement and addition of exercise and example numbers for GS-new.hs.

--- _my answers/_haskell road/_ch01/GS-new.hs
+++ _my answers/_haskell road/_ch01/GS-new.hs
@@ -1,18 +1,31 @@
+-- Example 1.8
+
 mnmInt :: [Int] -> Int
 mnmInt [] = error "empty list" 
 mnmInt [x] = x
 mnmInt (x:xs) = min x (mnmInt xs)
+
+min' :: Int -> Int -> Int 
+min' x y | x <= y    = x
+         | otherwise = y
+
+-- Exercise 1.9
 
 maxInt :: [Int] -> Int
 maxInt [] = error "empty list" 
 maxInt [x] = x
 maxInt (x:xs) = max x (maxInt xs)
 
-min' :: Int -> Int -> Int 
-min' x y | x <= y    = x
-         | otherwise = y
+-- Exercise 1.10
 
 removeFst :: Int -> [Int] -> [Int]
 removeFst r [] = []
 removeFst r (x:xs) | r      == x  = xs
                    | r      /= x  = x:removeFst r xs
+
+-- Example 1.11
+
+srtInts :: [Int] -> [Int]
+srtInts [] = []
+srtInts xs = m : (srtInts (removeFst m xs)) where m = mnmInt xs
+
