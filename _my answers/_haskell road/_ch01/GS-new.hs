547f7b8c7aa5fc01b68391a6e5db6d5d284f9b39
Jeff Lindstrom <jeff@jefflindstrom.com>
2013-10-26 00:24:15 -0700

    Example 1.12 doesn't work due to deprecated fromInt, yet length returns value Int.

--- _my answers/_haskell road/_ch01/GS-new.hs
+++ _my answers/_haskell road/_ch01/GS-new.hs
@@ -29,3 +29,38 @@ srtInts :: [Int] -> [Int]
 srtInts [] = []
 srtInts xs = m : (srtInts (removeFst m xs)) where m = mnmInt xs
 
+srtInts' :: [Int] -> [Int] 
+srtInts' [] = []
+srtInts' xs = let
+                m = mnmInt xs
+              in m : (srtInts' (removeFst m xs))
+
+-- Example 1.12
+
+-- average :: [Int] -> Float
+average [] = error "empty list"
+average xs = sum xs / fromIntegral (length xs)
+
+{- COMPLICATION
+
+Much to my frustration, I could not get this example to work "as is."
+
+The problem seems to be that length returns an Int value, but fromInt is now deprecated, and I couldn't figure how to import it.
+
+In the end removing the type declaration "solved" the problem, but I'm not too happy about it.
+
+Here's the deprecation note:
+
+http://www.haskell.org/ghc/docs/4.08.2/set/sec-int.html
+
+The following functions are deprecated in the new FFI. Do not use the following functions if you are interested in portability. Most of these functions are here for legacy reasons and may just vanish one day. You have been warned.
+
+The Int module also exports the overloaded operations for converting to and from Haskell Ints. However, for coercing between various integer types, better use fromIntegral, which is specialized for all the common cases so should be fast enough.
+
+toInt   :: (Integral a) => a -> Int
+fromInt :: (Num a) => Int -> a
+
+Portability note: both Hugs98 and all releases of GHC prior to ghc-4.05 also exports these two via the Prelude. So, to have code that uses toInt and fromInt be maximally portable, make sure you add an import on Int (even if the version of Hugs or GHC you're currently using may not export these two from there.)
+
+-}
+
